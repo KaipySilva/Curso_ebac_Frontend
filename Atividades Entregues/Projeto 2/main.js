5dba31f7da98551da3394d99c5b89a79445a6918
Kaipy <Kaipysilva91@gmail.com>
2024-08-27 19:21:11 -0300

    CSS finalizado

--- Atividades Entregues/Projeto 2/main.js
+++ Atividades Entregues/Projeto 2/main.js
@@ -15,11 +15,13 @@ function adicionaLinha(){
     const inputCadastroNome = document.getElementById('CadastroNome');
     const inputCpf = document.getElementById('CadastroCpfCnpj');
     const inputNumero = document.getElementById('CadastroNumero');
+    const pendente = 'Pendente'
 
     let linha = '<tr>'
     linha += `<td>${inputCadastroNome.value}</td>`;
     linha += `<td>${inputCpf.value}</td>`;
     linha += `<td>${inputNumero.value}</td>`;
+    linha += `<td>${pendente}</td>`;
     linha += '</tr>';
 
     linhas += linha;
